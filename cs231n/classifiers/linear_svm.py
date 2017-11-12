correct gradient with regularization

cs231n/classifiers/linear_svm.py
@@ -33,8 +33,8 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
-        dW[:,j] += X[i].transpose()
-        dW[:,y[i]] -= X[i].transpose()
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
         loss += margin
 
   # Right now the loss is a sum over all training examples, but we want it
@@ -44,7 +44,7 @@ def svm_loss_naive(W, X, y, reg):
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
-  dW += reg * np.sum(W)
+  dW += (reg * W)
 
   
 